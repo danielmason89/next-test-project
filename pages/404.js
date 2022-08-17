33710bc60f0ab64501c4b6697390585c3afa4013
Daniel Mason <danmasson0@gmail.com>
2022-08-16 22:03:56 -0400

    implement useEffect and useRouter to redirect user to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 1000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Oops....</h1>
