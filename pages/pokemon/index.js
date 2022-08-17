2330aace8b20109d58c80ef8d323bd02a30f0c46
Daniel Mason <danmasson0@gmail.com>
2022-08-16 23:30:53 -0400

    fecthed data from a dummy RESTApi to practise testing endppoints with a next.js, and provided styles for rendered elements

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -1,5 +1,26 @@
-const Pokemon = () => {
-  return <div>All Pokemon</div>;
+import styles from "../../styles/Pokemon.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { pokemon: data },
+  };
+};
+
+const Pokemon = ({ pokemon }) => {
+  return (
+    <div>
+      <h1>All Pokemon</h1>
+      {pokemon.map((poke) => (
+        <div key={poke.id}>
+          <a className={styles.single}>
+            <h3>{poke.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Pokemon;
