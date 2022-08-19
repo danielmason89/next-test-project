157fc739ccf71d1d6a73ddd2d7206f8f2d9e1931
Daniel Mason <danmasson0@gmail.com>
2022-08-18 23:00:46 -0400

    fetched a single item from a RESTApi, yayy we did it.

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -2,11 +2,10 @@ import Pokemon from ".";
 
 export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json;
-
-  const paths = data.map((ninjs) => {
+  const data = await res.json();
+  const paths = data.map((poke) => {
     return {
-      params: { id: Pokemon.id.toString() },
+      params: { id: poke.id.toString() },
     };
   });
   return {
@@ -15,10 +14,22 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+  return {
+    props: { poke: data },
+  };
+};
+
+const Details = ({ poke }) => {
   return (
     <div>
-      <h1>Details</h1>
+      <h1>{poke.name}</h1>
+      <p>{poke.email}</p>
+      <p>{poke.website}</p>
+      <p>{poke.address.city}</p>
     </div>
   );
 };
